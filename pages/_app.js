1e86da17b7128bf55adc37cec00eab4ea7036f1a
zoheir kabuli <zoheyrk@gmail.com>
2023-02-27 13:10:58 +0330

    add logo and icon, add styles things

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,26 @@
 import 'normalize.css';
 import '@/styles/globals.css';
 import { Vazirmatn } from 'next/font/google';
+import { ThemeProvider } from '@emotion/react';
 
+// * font
 const vazir = Vazirmatn({ subsets: ['arabic'], weight: 'variable' });
+
+// * theme
+const theme = {
+  colors: {
+    colorPrimary: '#fec200',
+    colorSecondary: '#f4cf4b',
+    colorBg: '#d1d2d64f',
+    colorDarkerBg: '#d9d9d9',
+    colorNightBg: '#1e2f47',
+    colorNightDarkerBg: '#1e2f47',
+  },
+  size: {
+    width: '91.666667%',
+    maxWidth: '128rem',
+  },
+};
 
 export default function App({ Component, pageProps }) {
   return (
@@ -12,7 +30,9 @@ export default function App({ Component, pageProps }) {
           font-family: ${vazir.style.fontFamily};
         }
       `}</style>
-      <Component {...pageProps} />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
     </>
   );
 }
