a0eeb1979394b5aadc194c048be21e14959c42bb
zoheir kabuli <zoheyrk@gmail.com>
2023-02-27 12:38:21 +0330

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,18 @@
-import '@/styles/globals.css'
+import 'normalize.css';
+import '@/styles/globals.css';
+import { Vazirmatn } from 'next/font/google';
+
+const vazir = Vazirmatn({ subsets: ['arabic'], weight: 'variable' });
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>{`
+        body {
+          font-family: ${vazir.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  );
 }
